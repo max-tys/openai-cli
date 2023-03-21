3b944b7b32dc8abb2b9a6caa823232a490b5dc4f
Max Tan <yenseow@gmail.com>
2023-03-21 11:33:15 -0400

    usage example

--- app.rb
+++ app.rb
@@ -24,8 +24,5 @@ def generate_text(prompt)
   puts response.dig('choices', 0, 'message', 'content')
 end
 
-# generate_text("How do I cite the Bank Secrecy Act in accordance with the Bluebook?")
-
-# generate_text("Turn this sentence into a legal disclaimer for my website.\nI am not a lawyer, and nothing on this website constitutes legal advice.")
-
-generate_text("Improve this github readme\n1. Get your API key from https://platform.openai.com/account/api-keys. 2. Save the API key in a .env file in your project folder. 3. Start chatting with ChatGPT with the generate_text method, passing in your prompt as an argument to the method.")
+# Usage example
+generate_text("How do I cite the Bank Secrecy Act in accordance with the Bluebook?")
